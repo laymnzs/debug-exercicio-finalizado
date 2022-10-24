7f733b5a752fb4efdcf0ce9db38fbda5baf43431
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2022-10-24 19:51:11 -0300

    Exercicio debug

---
+++ debug-react-exercicio-criacao-exercicio-debug/debug-react-exercicio-criacao-exercicio-debug/src/components/CardGrande/CardGrande.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components'
+
+import {LayoutCardGrande, ImgCardGrande, InfosCardGrande,TituloCardGrande} from "./styles"
+
+
+function CardGrande(props) {
+
+   
+    return (
+        <LayoutCardGrande>
+            <ImgCardGrande src={ props.imagem } />
+            <InfosCardGrande>
+                <TituloCardGrande>{ props.nome }</TituloCardGrande>
+                <p>{ props.descricao }</p>
+            </InfosCardGrande>
+        </LayoutCardGrande>
+    )
+}
+
+export default CardGrande
---
+++ debug-react-exercicio-criacao-exercicio-debug/debug-react-exercicio-criacao-exercicio-debug/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { LayoutCardPequeno, TituloCardPequeno, InfosCardPequeno, ImgCardPequeno} from "./styles"
+
+function CardPequeno(props) {
+
+   return(
+    
+        <LayoutCardPequeno>
+            <ImgCardPequeno src={props.imagem} />
+            <InfosCardPequeno>
+                <TituloCardPequeno>{props.nome}</TituloCardPequeno>
+                <p>{props.descricao}</p>
+            </InfosCardPequeno>
+        </LayoutCardPequeno>
+        )
+}
+
+export default CardPequeno
